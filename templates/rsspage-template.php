60350998fbb033c1da1fdfcad079fb9e9bfa6e5f
esteban <esteban@et-lin-i7.NETMDP>
2016-01-27 16:42:41 -0300

    test

---
+++ templates/rsspage-template.php
@@ -0,0 +1,10 @@
+<?php 
+/* 
+ * Template Name: for fcurrie with OPTIMIZEPRESS theme 
+ */ 
+?>
+<?php //op_theme_file('header') ?>
+<?php get_header(); ?>
+<?php the_content() ?>
+<?php get_footer(); ?>
+<?php //op_theme_file('footer') ?>
